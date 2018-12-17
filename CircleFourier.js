4b12ffe4901ad235d6050832bc42c0ebd34f3bf3
Tweety79rw <ryan.wilson2@email.wsu.edu>
2018-12-17 01:36:17 -0800

    added some comments and cleaned the code a little

--- CircleFourier.js
+++ CircleFourier.js
@@ -1,19 +1,34 @@
 class CircleForier {
-  constructor(r, iteration, maxIteration, N, fourier) {
+  /**
+   * The constructor
+   * @param {Number} iteration    The index of the recursion
+   * @param {Number} maxIteration The number of recursion, if null it uses iteration.
+   * @param {Function} N            This function calculates the n in the fourier series ie. 4 * sin(n * theta)/(n * PI)
+   * @param {Function} fourier      This function calculates the the r for this recursion using this object's n variable
+   */
+  constructor(iteration, maxIteration, N, fourier) {
     this.angle = 0;
     this.iteration = iteration || 1;
     this.maxIteration = maxIteration || this.iteration;
     this.n = N(this.N());
     this.r = fourier(this.n);
     if(this.iteration > 1) {
-      this.child = new CircleForier(r, this.iteration - 1, this.maxIteration, N, fourier);
+      this.child = new CircleForier(this.iteration - 1, this.maxIteration, N, fourier);
     } else if(this.iteration === 1) {
       this.wave = [];
     }
   }
+  /**
+   * Calculates the index for this recursion 0 for the first and maxIteration for the last
+   */
   N() {
     return this.maxIteration - this.iteration;
   }
+  /**
+   * Renders the visual on a canvas
+   * @param {Number} cx The center x position for this circle to be drawn
+   * @param {Number} cy The center y position for this circle to be drawn
+   */
   Render(cx, cy) {
     let x = cx + this.r * cos(this.n * this.angle);
     let y = cy + this.r * sin(this.n * this.angle);
--- sketch.js
+++ sketch.js
@@ -2,10 +2,12 @@ let cf;
 let cf2;
 function setup() {
   createCanvas(1600,800);
-  cf = new CircleForier(200, 30, null,function(n) { return n+1;}, function(n) { return 200 * (2 / ((n%2===0?n:-n) * PI));});
-  //cf = new CircleForier(200, 30, null,function(n) { return n + 3;}, function(n) { return 200 * (2 / ((n) * PI));});
-  cf2 = new CircleForier(200, 30, null, function(n) {return n * 2 + 1;}, function(n) { return 200 * (4 / (n * PI));});
+  // Sawtooth fourier series n starts a 1 and increments by 1
+  cf = new CircleForier(30, null,function(n) { return n+1;}, function(n) { return 200 * (2 / ((n%2===0?n:-n) * PI));});
+  // squar wave fourier series n starts at 1 and increments by 2 making all values odd
+  cf2 = new CircleForier(30, null, function(n) {return n * 2 + 1;}, function(n) { return 200 * (4 / (n * PI));});
 }
+
 function draw() {
   background(0);
   cf.Render(400,400);
